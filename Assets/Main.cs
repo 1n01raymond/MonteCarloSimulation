3b4a245246b7fe7f14fbac9bc38f3c9cc87b68fc
DESKTOP-RAYMOND\Raymond <1n01raymond@gmai.com>
2021-03-24 01:10:24 +0900

    default drawing complete

--- Assets/Main.cs
+++ Assets/Main.cs
@@ -1,14 +1,36 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
 
 public class Main : MonoBehaviour
 {
-    const float DEG2RAD = 3.14159f/180;
+    
+    private const float DEG2RAD = 3.14159f/180;
+    
+    [SerializeField] private Text sampleCountInput;
+    [SerializeField] private Text resultText;
+    [SerializeField] private Material mat;
+
+    private List<Vector2> samplePosList = new List<Vector2>();
+    private List<Vector2> samplePosInCircleList = new List<Vector2>();
+
+    private float squareWidth;
+    private float squareHeight;
+    private float circleRadius = 100f;
+    
+    void Awake()
+    {   
+        squareWidth = 100f / Screen.width;
+        squareHeight = 100f / Screen.height;
+    }
     
     void OnPostRender()
     {
         GL.PushMatrix();
+        mat.SetPass (0);
         GL.LoadOrtho();
 
         // x line
@@ -25,27 +47,19 @@ public class Main : MonoBehaviour
         GL.Vertex(new Vector3( 0.5f, 1, 0));
         GL.End();
         
+        
         // square
-        var squreWidth = 100f / Screen.width;
-        var squreHeight = 100f / Screen.height;
-        
-        GL.Begin(GL.LINES);
-        GL.Color(Color.black);
-        
-        GL.Vertex(new Vector3( 0.5f, 0.5f + squreHeight, 0));
-        GL.Vertex(new Vector3( 0.5f + squreWidth, 0.5f + squreHeight, 0));
-        
-        GL.Vertex(new Vector3( 0.5f + squreWidth, 0.5f + squreHeight, 0));
-        GL.Vertex(new Vector3(0.5f + squreWidth, 0.5f, 0));
-        GL.End();
+        DrawSquare(new Vector2(0.5f + squareWidth / 2, 0.5f + squareHeight / 2), squareWidth, squareHeight, Color.black);
         
         // circle
-        DrawCircle(new Vector2(0.5f, 0.5f), 100f);
+        DrawCircle(new Vector2(0.5f, 0.5f), circleRadius);
 
-        for (int i = 0; i < 100; i++)
-        {
-            DrawCircle(new Vector2(Random.Range(0.5f, 0.5f + squreWidth), Random.Range(0.5f, 0.5f + squreHeight)), 1f);
-        }
+        // samples
+        foreach (var pos in samplePosList)
+            DrawSquare(pos, 0.001f, 0.001f, Color.red);
+        
+        foreach (var pos in samplePosInCircleList)
+            DrawSquare(pos, 0.001f, 0.001f, Color.blue);
         
         GL.PopMatrix();
     }
@@ -70,5 +84,61 @@ public class Main : MonoBehaviour
             GL.Vertex(new Vector3(x, y, 0));
         }
         GL.End();
+    }
+
+    private void DrawSquare(Vector2 center, float width, float height, Color color)
+    {
+        GL.Begin(GL.LINES);
+        GL.Color(color);
+        
+        GL.Vertex(new Vector3( center.x - width / 2, center.y + height / 2, 0));
+        GL.Vertex(new Vector3( center.x + width / 2, center.y + height / 2, 0));
+        
+        GL.Vertex(new Vector3( center.x + width / 2, center.y + height / 2, 0));
+        GL.Vertex(new Vector3( center.x + width / 2, center.y - height / 2, 0));
+        
+        GL.Vertex(new Vector3( center.x + width / 2, center.y - height / 2, 0));
+        GL.Vertex(new Vector3( center.x - width / 2, center.y - height / 2, 0));
+        
+        GL.Vertex(new Vector3( center.x - width / 2, center.y - height / 2, 0));
+        GL.Vertex(new Vector3( center.x - width / 2, center.y + height / 2, 0));
+        
+        
+        GL.End();
+    }
+
+    public void OnClickDraw()
+    {
+        int sampleCount = 1;
+        int sampleInCircle = 0;
+        try
+        {
+            sampleCount = Int32.Parse(sampleCountInput.text);
+        }
+        catch (Exception e)
+        {
+            
+        }
+        
+        samplePosList.Clear();
+        samplePosInCircleList.Clear();
+        
+        for (int i = 0; i < sampleCount; i++)
+        {
+            var samplePos = new Vector2(Random.Range(0.5001f, 0.5f + squareWidth), Random.Range(0.5001f, 0.5f + squareHeight));
+
+            if (Vector2.Distance(samplePos, new Vector2(0.5f, 0.5f)) < 
+                Vector2.Distance(new Vector2(circleRadius / Screen.width, circleRadius / squareHeight), new Vector2(0.5f, 0.5f)))
+            {
+                samplePosInCircleList.Add(samplePos);
+                sampleInCircle++;
+            }
+            else
+            {
+                samplePosList.Add(samplePos);
+            }
+        }
+
+        resultText.text = samplePosInCircleList.Count + " / " + sampleCount;
     }
 }
